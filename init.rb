82a5a5076e8c1989a2bceb93f1c6784ffbeecd9e
Alexey Kuleshov <kulesa@gmail.com>
2011-03-22 16:25:08 +0300

    Files menu added

--- init.rb
+++ init.rb
@@ -19,6 +19,6 @@ Redmine::Plugin.register :redmine_custom do
   
   menu :top_menu, :all_tasks, {:controller => 'issues', :action => 'index' }, :caption => 'All tasks', :after => :projects
   menu :top_menu, :gantt, {:controller => 'issues/gantt', :action => 'index' }, :caption => 'Gantt', :after => :all_tasks
-#  menu :top_menu, :files, {:controller => 'files', :action => 'index' }, :caption => 'Files', :after => :gantt
+  menu :top_menu, :files, {:controller => 'files', :action => 'index' }, :caption => 'Files', :after => :gantt
   menu :top_menu, :calendar, {:controller => 'issues/calendar', :action => 'index' }, :caption => 'Calendar', :after => :files
 end
