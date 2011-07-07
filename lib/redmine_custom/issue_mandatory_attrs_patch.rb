559d5a7fe98b8c334b9a83e5bec89b3d37cfdad2
Alexey Kuleshov <kulesa@gmail.com>
2011-07-07 11:29:39 +0400

    Issue validates presense of Assignee

--- lib/redmine_custom/issue_mandatory_attrs_patch.rb
+++ lib/redmine_custom/issue_mandatory_attrs_patch.rb
@@ -2,7 +2,7 @@ module RedmineCustom
   module IssueMandatoryAttrsPatch
     def self.included(base) # :nodoc:
       base.class_eval do
-        validates_presence_of :due_date
+        validates_presence_of :due_date, :assigned_to
       end
     end
   end
